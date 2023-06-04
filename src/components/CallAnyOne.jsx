60a01a22f5451b96c77c4bb50a54e90e91029f56
오형준 <hungry6459@gmail.com>
2023-06-04 19:02:42 +0900

    전화걸기 수정

--- src/components/CallAnyOne.jsx
+++ src/components/CallAnyOne.jsx
@@ -4,8 +4,8 @@ export default function CallAnyOne({ props }) {
     return(
         <div className={styles.callWrap}>
             <span className={styles.callName}>{props.name}에게 연락하기</span>
-            <span className={styles.callTel}><a href={`"tel:${props.tel}"`}><img src="/images/call_icon.png" width="32" alt="전화 아이콘"/></a></span>
-            <span className={styles.callSMS}><a href={`"sms:${props.tel}"`}><img src="/images/sms_icon.png" width="36" alt="SMS 아이콘"/></a></span>
+            <span className={styles.callTel}><a href={`tel:${props.tel}`}><img src="/images/call_icon.png" width="32" alt="전화 아이콘"/></a></span>
+            <span className={styles.callSMS}><a href={`sms:${props.tel}`}><img src="/images/sms_icon.png" width="36" alt="SMS 아이콘"/></a></span>
         </div>
 
     );
