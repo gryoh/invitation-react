fe3d2e75356b130ae54d80394ee5256ad6dac7a7
오형준 <hungry6459@gmail.com>
2023-06-04 18:25:56 +0900

    인트로 영역 생성

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,41 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className={[`${styles.section} ${styles.intro}`]}>
+            <img src="/images/intro.png"/>
+            <div className={styles.pWrap}>
+                <p className={styles.name}>오형준</p>
+                <p className={styles.name}>조한미</p>
+                <p className={styles.hallFirst}>2024. 04. 20. SAT PM 4 :00</p>
+                <p className={styles.hall}>라마다 서울 신도림 호텔 5층 세인트그레이스홀</p>
+            </div>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className={[`${styles.section} ${styles.message}`]}>
+            <p className={styles.message_title}>초대합니다</p>
+            <div className={styles.message_content}>
+                <p>새로운 마음과 새 의미를 간직하며</p>
+                <p>저희 두 사람이 새 출발의</p>
+                <p>첫걸음을 내딛습니다.</p>
+                <p>좋은 꿈, 바른 뜻으로 올바르게 살 수 있도록</p>
+                <p>축복과 격려 주시면</p>
+                <p>더없는 기쁨으로 간직하겠습니다.</p>
+            </div>
+        </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <div className={[`${styles.section} ${styles.call}`]}>
+            전화하기
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        <div>갤러리</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+        <div>찾아오시는길</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div>축하의 한마디씩</div>
     </main>
   )
 }
